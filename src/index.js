29c3372cdff0431383e501e18e2bdfc7d51d72db
jhernandez <jhernandez@MDC-TIC-0796>
2018-10-26 15:46:36 -0500

    modificacion de app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const express=require("express");
 const path=require('path');
 const app=express();
 
-//const IndexRoute=require('./routes/index');
-const TaskRoute=require('./routes/task');
+//const IndexRoute=require('.//index');
+//const TaskRoute=require('./routes/task');
 //setting
-app.set('views',path.join(__dirname,'views'));
+//app.set('views',path.join(__dirname,'views'));
 app.set('port',process.env.PORT || 3000);
 app.engine('html',require('ejs').renderFile);
 app.set('view engine','ejs');
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 //routes
 //app.use(IndexRoute);
-app.use('/api',TaskRoute);
+//app.use('/api',TaskRoute);
 
 //static file
-app.use(express.static(path.join(__dirname,'views')));
+app.use(express.static(path.join(__dirname,'dist')));
 
 //start server
 app.listen(app.get('port'),()=>{
